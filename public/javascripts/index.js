1931882b3f039017d5297fc5a2e5526f42aaaeed
jasper <reyno511@umn.edu>
2016-10-01 17:29:00 -0500

    button updates

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,3 +4,12 @@ $('#onSwitch').click(function () {
 $('#offSwitch').click(function () {
 	$.post('http://10.0.0.183/off');
 })
+$('#buzzHigh').click(function () {
+	$.post('http://10.0.0.183/buzzHigh');
+})
+$('#buzzLow').click(function () {
+	$.post('http://10.0.0.183/buzzLow');
+})
+$('#buzzRand').click(function () {
+	$.post('http://10.0.0.183/buzzRand');
+})
